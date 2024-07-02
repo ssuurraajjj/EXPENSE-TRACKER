022a9c2bd6f22adf6f4087357c5d48d5ab887781
unknown <suraj9335169159@gmail.com>
2024-07-02 11:45:23 +0530

    commited

---
+++ my-react-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Header } from './components/header';
+import { Balance } from './components/balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
